b7506aa0c58d0c7d8da72285b43a7c368196434e
maxsean <xsma1990@gmail.com>
2017-12-13 12:37:18 -0500

    initiated game controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::GamesController < Api::V1::ApiController
 
   def show
+    game = Game.find(params["id"]).to_json(include: :word)
+
+    render json: game
   end
-  
+
+  def create
+    body = JSON.parse(request.body.read)
+    game = Game.new(body)
+    #set randome word as game word, database has unique so if it doesn't save, get another (until loop)
+
+    #return list of games to user?
+  end
+
 end
