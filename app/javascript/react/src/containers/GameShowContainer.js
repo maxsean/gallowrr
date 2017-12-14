4c960badef68d950d2034444c2a8b6fd2a879122
maxsean <xsma1990@gmail.com>
2017-12-14 14:45:23 -0500

    initialize game page with fetch calls

--- app/javascript/react/src/containers/GameShowContainer.js
+++ app/javascript/react/src/containers/GameShowContainer.js
@@ -9,21 +9,62 @@ class GameShowContainer extends React.Component{
       incorrect: 0,
       chosen_letters: [],
       word: null,
+      game_user_id: null,
+      current_user_id: null
     }
+    this.fetchGame = this.fetchGame.bind(this)
+    this.fetchCurrentUser = this.fetchCurrentUser.bind(this)
   }
 
   componentDidMount(){
+    this.fetchCurrentUser()
+    this.fetchGame()
+  }
+
+  fetchCurrentUser(){
+    fetch('/api/v1/users.json', {
+      credentials: 'same-origin',
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.setState({ current_user_id: data.user.id });
+    })
+  }
+
+  fetchGame(){
     fetch(`/api/v1/games/${this.props.params.id}`)
     .then(response => response.json())
     .then(data => {
-      debugger;
+      this.setState({
+        incorrect: data.incorrect,
+        chosen_letters: data.chosen_letters,
+        word: data.word.body,
+        game_user_id: data.user_id
+      })
     })
   }
 
   render(){
+    let display =
+    <h1>You are not authorized to view this game</h1>
+
+    if(this.state.game_user_id == this.state.current_user_id){
+      display =
+      <div>
+        <HangmanContainer
+          incorrect={this.state.incincorrect}
+        />
+        <AlphabetContainer
+          chosen_letters={this.state.chosen_letters}
+          word={this.state.word}
+        />
+      </div>
+    }
     return(
       <div>
-
+        {display}
       </div>
     )
   }
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@ csv = CSV.parse(csv_text, :encoding => 'ISO-8859-1')
 csv.each do |row|
   Word.create!(body: "#{row[0].chomp}")
 end
+
+user1 = User.create!(handle: "testuser", email: "test@user.com", password: "123456", password_confirmation: "123456")
+
+user2 = User.create!(handle: "fakeuser", email: "fake@user.com", password: "123456", password_confirmation: "123456")
+
+game1 = Game.create!(user: user1, word_id: 2, incorrect: 0, complete: false, chosen_letters: [])
+
+game2 = Game.create!(user: user1, word_id: 3, incorrect: 0, complete: false, chosen_letters: [])
