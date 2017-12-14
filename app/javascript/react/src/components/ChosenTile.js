9fd4acd634c478310eb3e0e217489738d2a0585a
maxsean <xsma1990@gmail.com>
2017-12-14 18:48:50 -0500

    choosing letter available list now updates database

---
+++ app/javascript/react/src/components/ChosenTile.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+class ChosenTile extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      chosen_letters: this.props.chosen_letters
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+  this.setState({ chosen_letters: nextProps.chosen_letters });
+  }
+
+  render(){
+    let letters;
+    if(this.state.chosen_letters){
+      letters = this.state.chosen_letters.map(letter => {
+        return(
+          <div className="letter-tile"
+            key={String(Date.now()) + '-' + Math.floor(Math.random() * 10000)}>
+              {letter}
+            </div>
+        )
+      })
+    }
+
+    return(
+      <div>
+        {letters}
+      </div>
+    )
+  }
+}
+
+export default ChosenTile
--- app/javascript/react/src/containers/AlphabetContainer.js
+++ app/javascript/react/src/containers/AlphabetContainer.js
@@ -1,15 +1,66 @@
 import React from 'react'
+import SecretWordContainer from './SecretWordContainer'
+import ChosenTile from '../components/ChosenTile'
+import AvailableTile from '../components/AvailableTile'
 
 class AlphabetContainer extends React.Component{
   constructor(props){
     super(props)
     this.state = {
+      available_letters: []
+    }
+    this.determineAvailableLetters = this.determineAvailableLetters.bind(this)
+  }
 
+  componentDidMount(){
+    this.determineAvailableLetters()
+  }
+
+  componentWillReceiveProps(nextProps) {
+  this.determineAvailableLetters();
+}
+
+
+  determineAvailableLetters(){
+    let alphabet_string = 'abcdefghijklmnopqrstuvwxyz'
+    let alphabet_array = alphabet_string.toUpperCase().split('')
+    let available_letters = []
+
+    if(this.props.chosen_letters.length > 0){
+      for(let i = 0; i < alphabet_array.length; i++){
+        if(!this.props.chosen_letters.includes(alphabet_array[i])){
+          available_letters.push(alphabet_array[i])
+        }
+      }
+      this.setState({
+        available_letters: available_letters
+      })
+    } else {
+      this.setState({
+        available_letters: alphabet_array
+      })
     }
   }
+
   render(){
     return(
-      <div>
+      <div className="alphabet-container">
+        <hr/>
+        <SecretWordContainer
+          word={this.props.word}
+          chosen_letters={this.props.chosen_letters}
+        />
+        <hr/>
+        <ChosenTile
+          chosen_letters={this.props.chosen_letters}
+        />
+        <hr/>
+
+        <AvailableTile
+          letters={this.state.available_letters}
+          handleClick={this.props.handleClick}
+        />
+        <hr/>
 
       </div>
     )
