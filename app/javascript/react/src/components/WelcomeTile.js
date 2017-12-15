b25723242a1a18cfdb6b3cb4ddae4f366a146106
maxsean <xsma1990@gmail.com>
2017-12-15 00:55:06 -0500

    changed class for styling

--- app/javascript/react/src/components/WelcomeTile.js
+++ app/javascript/react/src/components/WelcomeTile.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WelcomeTile = (props) => {
   return(
-    <div className="welcome-tile">
+    <div className="game-tile">
       <h1>Welcome to GallowRR</h1>
       <p>A simple hangman app built using Ruby on Rails and React.js</p>
       <p>Please sign in or sign up</p>
